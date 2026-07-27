9fa3206edafd546ec6e2c31abadbc159263230ea
iloi21 <seabugs078@gmail.com>
2026-07-26 17:37:38 -0700

    checkdate not working

--- todo.py
+++ todo.py
@@ -1,2 +1,32 @@
-# a;sdkfjadsf
-# hi
+import os
+import sys
+from datetime import datetime
+
+class task:
+    def __init__(self):
+        self.task = {}
+
+    def checkDate(self, date):
+        try:
+            datetime.strptime(date, '%Y-%m-%d')
+            return True
+        except ValueError:
+            return False
+
+    def checkTime(self, time):
+        try:
+            datetime.strptime(time, '%H:%M')
+            return True
+        except ValueError:
+            return False
+
+    def setDate(self, date, info):
+        if self.checkDate(date):
+            self.task[date] = info
+            print(f"Task added: {info} (Due by: {date})")
+        else:
+            print("Invalid date format. Please use YYYY-MM-DD.")
+        
+
+
+    
