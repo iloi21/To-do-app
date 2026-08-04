1052d03adbaa0b99e9d668e5afc91a635a226ace
iloi21 <seabugs078@gmail.com>
2026-08-04 14:19:03 -0700

    added a reward system

--- todo.py
+++ todo.py
@@ -5,6 +5,7 @@ from datetime import datetime
 class Task:
     def __init__(self):
         self.task = {}
+        self.flowers = 0
 
     def checkDate(self, date):
         try:
@@ -52,6 +53,8 @@ class Task:
             if not self.task[date]:  # If no tasks left for the date, remove the date entry
                 del self.task[date]
             print(f"Task for {date} at {time} marked as completed.")
+            print("You have earned a flower for completing a task!")
+            self.flowers += 1
         else:
             print(f"No task found for {date} at {time}.")
 
@@ -65,3 +68,13 @@ class Task:
             for time in sorted(self.task[date].keys()):
                 desc = self.task[date][time]["description"]
                 print(f"  {time} - {desc}")
+
+    def checkFlowers(self):
+        if self.flowers < 10:
+            print(f"You have {self.flowers} flowers. Keep completing tasks to earn more!")
+        elif self.flowers == 10:
+            print("Great job! you have 10 flowers! You made yourself a bouquet! Keep earning more to create a garden (20+ flowers)! XD")
+        elif self.flowers == 20:
+            print("Wow! you have 20 flowers! You made yourself a garden! Keep earning more to create a field (50+ flowers)! XD")
+        elif self.flowers >= 50:
+            print(f"Amazing! you have {self.flowers} flowers! You made yourself a field! Keep earning more to create a forest (100+ flowers)! XD")
